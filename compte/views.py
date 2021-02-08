7a02518970ebd9d7936712d764275bac6211bc46
AbaaHans <brownshans@gmail.com>
2021-02-08 18:43:23 +0100

    message erreur

--- compte/views.py
+++ compte/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate,login
+from django.contrib import messages
 from .forms import CreeUtilisateur
 
 # Create your views here.
@@ -12,6 +13,8 @@ def Inscription(request):
         form=CreeUtilisateur(request.POST)
         if form.is_valid():
             form.save()
+            user=form.cleaned_data('username')
+            messages.success(request,'Compte créer avec succès')
             return redirect('login')
     context = {'form':form}
     return render(request,'compte/inscription.html',context)
@@ -26,4 +29,6 @@ def Login(request):
         if user is not None:
             login(request,user)
             return redirect('home')
+        else:
+            messages.info(request, 'Identifiant/Mot de passe incorrect')
     return render(request,'compte/login.html',context)
